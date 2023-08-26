b8ac1a13102a75e86825800695be6c381748b3bc
Pedro <pedrogoku80@gmail.com>
2023-08-26 11:50:19 -0300

    Implementation in newRegister

--- week-2/models/userModels.js
+++ week-2/models/userModels.js
@@ -5,6 +5,19 @@ const getAll =  async() => {
     return user
 }
 
+const newRegister =  async(user) => {
+    const {name, email, password} = user;
+    const query = 'INSERT INTO login(name, email, password) VALUES (?, ?, ?)'
+    const [createdAccount] = await connection.execute(query, [name, email, password])
+    if (createdAccount.affectedRows === 1) { 
+        return { name, email } 
+    }else { 
+        throw new Error("Falha ao criar a conta") 
+    }  
+}
+
+
 module.exports = {
-    getAll
+    getAll,
+    newRegister
 }
