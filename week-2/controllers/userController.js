c7c5e9ebc9c5f4c1612b9eff6a8c3d4896552014
Pedro <pedrogoku80@gmail.com>
2023-08-26 11:46:09 -0300

    creating newRegister

--- week-2/controllers/userController.js
+++ week-2/controllers/userController.js
@@ -1,10 +1,16 @@
 const userModel = require('../models/userModels')
 
-const getAll =  async(req,res) => {
+const getAll =  async(_req,res) => {
     const user =  await userModel.getAll()
     return res.status(200).json(user);
 }
 
+const newRegister =  async(req,res) => {
+    const createdAccount = await userModel.newRegister(req.body)
+    return res.status(201).json(createdAccount)
+}
+
 module.exports = {
-    getAll
+    getAll,
+    newRegister
 }
