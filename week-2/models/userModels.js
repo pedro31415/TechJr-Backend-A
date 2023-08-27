014df7befaca6af73e9c922e2e4b2987343b0d45
Pedro <pedrogoku80@gmail.com>
2023-08-27 15:03:54 -0300

    delete and update

--- week-2/models/userModels.js
+++ week-2/models/userModels.js
@@ -16,8 +16,22 @@ const newRegister =  async(user) => {
     }  
 }
 
+const deleteAccount = async(id) =>{
+    const query = 'DELETE FROM login WHERE id = ?'
+    const removedAccount = await connection.execute(query, [id])
+    return removedAccount
+}
+
+const updateAccount = async(id,user) =>{
+    const query = 'UPDATE login SET name = ?, email = ?, password = ? WHERE id = ?'
+    const {name,email,password} = user
+    const updateAccount = await connection.execute(query, [name,email,password,id])
+    return updateAccount
+}
 
 module.exports = {
     getAll,
-    newRegister
+    newRegister,
+    deleteAccount,
+    updateAccount
 }
